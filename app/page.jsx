9d19b006209b004b898d76ff0f12d8695e777ca0
LukeLarson2 <lucas.m.larson2@gmail.com>
2023-08-19 19:04:28 -0700

    test on why not multiple users

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ export default function Page() {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    function getCookie(name) {
+    const getCookie = (name) => {
       const value = "; " + document.cookie;
       const parts = value.split("; " + name + "=");
       if (parts.length === 2) return parts.pop().split(";").shift();
@@ -17,39 +17,22 @@ export default function Page() {
     const pulledUser = getCookie("currentUser");
     const pulledCharacter = getCookie("selectedCharacterId");
 
+    console.log(pulledCharacter)
+    console.log(pulledUser)
+
     setCurrentUser(pulledUser);
     setSelectedCharacterId(pulledCharacter);
     setIsLoading(false);
   }, []);
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      if (selectedCharacterId) {
-        localStorage.setItem(
-          "characterData",
-          JSON.stringify(selectedCharacterId)
-        );
-      } else {
-        const validCharacter = localStorage.getItem("characterData")
-          ? JSON.parse(localStorage.getItem("characterData"))
-          : "";
-        if (validCharacter) {
-          setSelectedCharacterId(validCharacter);
-        }
-      }
-    }
-  }, [selectedCharacterId]);
-
-64
-
   if (isLoading) {
-    let divCounter = 0
-    for (let i = 0; i < 64; i++){
-
-    }
-    return <>
-<div>Loading...</div>
-    </>
+    let divCounter = 0;
+    for (let i = 0; i < 64; i++) {}
+    return (
+      <>
+        <div>Loading...</div>
+      </>
+    );
   }
 
   return (
